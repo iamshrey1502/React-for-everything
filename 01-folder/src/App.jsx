14614b7c0193374b316d785cf7aee11de081243f
Shrey Mishra <shreymishra1502@gmail.com>
2026-03-17 13:07:52 +0530

    Initial commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h2>Hello from App</h2>
+    </div>
+  )
+}
+
+export default App
+
+// const App=()=>{
+//   return <div id="parent">
+//     <h1>Hello World</h1>
+//     <h1>Hello from App</h1>
+//   </div>
+// };
+
+// export default App;
+
+
+
