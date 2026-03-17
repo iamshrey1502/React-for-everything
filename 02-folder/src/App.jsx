ad4b8937dfb85918f865b1c9149b9ff7cdc60a79
Shrey Mishra <shreymishra1502@gmail.com>
2026-03-17 21:15:31 +0530

    Components

---
+++ 02-folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from "./components/Nav"
+import Footer from "./components/Footer"
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <div className="hero">
+        
+      </div>
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default App
