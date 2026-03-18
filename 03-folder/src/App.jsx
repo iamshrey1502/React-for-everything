296569b8fe724edaa0be068a8eb73c57f6be45f9
Shrey Mishra <shreymishra1502@gmail.com>
2026-03-18 22:06:49 +0530

    Tailwind CSS

---
+++ 03-folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    
+    <div className='text-9xl font-bold bg-emerald-500 h-100 px-20 py-20 border-8'>
+      <div><Navbar/></div>
+    </div>
+  )
+}
+
+export default App
---
+++ 03-folder/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between '>
+        <h1 >Welcome</h1>
+        <h1>to</h1>
+        <h1>the</h1>
+        <h1>Suplex</h1>
+        <h1>City</h1>
+    </div>
+  )
+}
+
+export default Navbar
