25711d461e2e33452c8da763da11a082f37d8d54
Shrey Mishra <shreymishra1502@gmail.com>
2026-03-20 12:59:15 +0530

    Function calling

--- 05-folder/src/App.jsx
+++ 05-folder/src/App.jsx
@@ -6,14 +6,19 @@ import Women from './components/Women'
 
 
 const App = () => {
-  let gender="Male";
+
+  let btnClicked=()=>{
+    console.log("Clicked");
+  }
+  
+  
   return (
     <div>
-      {(gender==="Male")?<Men/>:<Women/>};
-      
+      <button 
+      onClick={btnClicked}
+      className='active:scale-90 bg-blue-400 text-bold text-black rounded-lg border-4 m-20 p-3'>Click me</button>
     </div>
   )
 }
 
 export default App
-//Conditional rendering
