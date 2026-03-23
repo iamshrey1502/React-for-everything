df5726aad70dbaaec94a9a7321a9febba8ba053e
Shrey Mishra <shreymishra1502@gmail.com>
2026-03-23 20:35:23 +0530

    Axios

--- 10-folder/src/App.jsx
+++ 10-folder/src/App.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from "axios";
 
 const App = () => {
-  return (
-    <>
-    
-    <h1>Hello World</h1>
-    <h1>App</h1>
 
-    </>//Fragments, used to group multiple elements without adding extra nodes to the DOM.
+  const [alldata, setAlldata] = useState([]);
+
+  const getData=async ()=>{
+    const response= await axios.get("https://picsum.photos/v2/list");// always return a promise
+    
+    let newdata=[...alldata,...response.data];
+    setAlldata(newdata);
+    console.log(response.data);
+    // console.log("Data Fetched");
+  }
+  return (
+    <div>
+      <button onClick={()=>{
+        getData();
+      }}>Get Data</button>
+
+      {alldata.map(function(val,idx){
+        return <h1 key={idx}>{val.author} {idx+1}</h1>
+      })}
+    </div>
   )
 }
 
 export default App
 
-//Data in React flow one way, from parent to child components. This is achieved through props, which are read-only and cannot be modified by the child component. 
-// Fragments dont support attibutes
+//CSR(Client Side Rendering) -> React, Angular, Vue
+//data comes in JSON format
+//slow 
+// Imporoves SEO(Search Engine Optimization)
+
+ 
+
+
+
+//SSR(Server Side Rendering)-> Next.js, Nuxt.js, EJS pages
+//data comes in HTML format
+//fast
+//low SEO
