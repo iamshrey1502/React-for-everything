65e1647289428efc32cdf050497e6892dd71ad10
Shrey Mishra <shreymishra1502@gmail.com>
2026-03-23 13:40:13 +0530

    Fragments

---
+++ 10-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    
+    <h1>Hello World</h1>
+    <h1>App</h1>
+
+    </>//Fragments, used to group multiple elements without adding extra nodes to the DOM.
+  )
+}
+
+export default App
+
+//Data in React flow one way, from parent to child components. This is achieved through props, which are read-only and cannot be modified by the child component. 
+// Fragments dont support attibutes
