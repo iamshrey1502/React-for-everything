be4b7f11f2ba7ec0ebb8c6be1e0e2ebfb519fd7f
Shrey Mishra <shreymishra1502@gmail.com>
2026-03-24 00:21:58 +0530

    useEffect 2

--- 11-folder/src/App.jsx
+++ 11-folder/src/App.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
+import axios from "axios";
 import {useEffect} from "react";
 import {useState} from "react";
 
 const App = () => {
 
+  const [alldata, setalldata] = useState([]);
   const [num, setnum] = useState(0);
-  const [title, settitle] = useState("");
 
-  useEffect(()=>{ // another hook in react used to execute things in sidestack.
-    console.log("running");
-  },[num]);// dependency array   
+  const getData=async ()=>{
+    const response= await axios.get("https://randomuser.me/api/");
+    // console.log(response.data.results[0].name.first);
+    setalldata(response.data.results[0].name.first);
+  }
+  useEffect(()=>{
+    getData();
+  },[num]);
   return (
     <div>
+      {alldata}
       <h1>{num}</h1>
       <button onClick={()=>{
         setnum(num+1);
-      }}
-      >Increase</button>
-      <input type="text" placeholder="Enter text" value={title} onChange={(e)=>{
-        settitle(e.target.value)
-      }} />
-
+      }}>
+      Click here to get random person
+      </button>
     </div>
   )
 }
